77605ecf3980ffe1292fca03914e4d473b91acdb
rubences <91731294+rubences@users.noreply.github.com>
2025-03-24 11:25:30 +0000

    cambios

--- ConversationFormatter.py
+++ ConversationFormatter.py
@@ -4,20 +4,21 @@ class ConversationFormatter:
 
     def __str__(self):
         return self.text
+
+   
     
-    
-    def format_conversation(self):
-        # Split the text by '#' to separate the sentences
-        sentences = self.text.split('#')
+def format_conversation(self):
+    # Split the text by '#' to separate the sentences
+    sentences = self.text.split('#')
         
-        # Capitalize the first letter of the first sentence
-        sentences[0] = sentences[0].capitalize() + "..."
+    # Capitalize the first letter of the first sentence
+    sentences[0] = sentences[0].capitalize() + "..."
         
-        # Capitalize the first letter of each subsequent sentence and add a period at the end
-        for i in range(1, len(sentences)):
-            sentences[i] = sentences[i][0].upper() + sentences[i][1:] + '.'
+    # Capitalize the first letter of each subsequent sentence and add a period at the end
+    for i in range(1, len(sentences)):
+        sentences[i] = sentences[i][0].upper() + sentences[i][1:] + '.'
         
-        # Join the sentences with new lines
-        formatted_text = '\n'.join(sentences)
+    # Join the sentences with new lines
+    formatted_text = '\n'.join(sentences)
         
-        return formatted_text
+    return formatted_text
